878f3f3dab4200603a9971053168331226f416c9
Michael Malak <m.malakrizk@gmail.com>
2020-11-30 15:40:28 +0200

    Design Home Page

--- StAbraamFamily/Controllers/HomeController.cs
+++ StAbraamFamily/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using StAbraamFamily.Models;
+using StAbraamFamily.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,30 @@ namespace StAbraamFamily.Controllers
 {
     public class HomeController : Controller
     {
+        private StAbraamEntities db = new StAbraamEntities();
+ 
+
         public ActionResult Home()
         {
-            return View();
+            SystemDashboard systemDashboard = new SystemDashboard()
+            {
+                Fathers = db.Fathers.Where(x => x.IsActive == true).ToList().Count(),
+                Servants = db.Servants.Where(x => x.IsActive == true).ToList().Count(),
+                Families = db.Families.Where(x => x.IsActive == true).ToList().Count(),
+                FamilyServants = db.FamilyServants.Where(x => x.IsActive == true).ToList().Count(),
+                BagServices = db.ServiceActions.Where(x => x.IsActive == true && x.ServiceType.ID == 1).ToList().Count(),
+                MedicalServices = db.ServiceActions.Where(x => x.IsActive == true && x.ServiceType.ID == 2).ToList().Count(),
+                ServiceActions = db.ServiceActions.Where(x => x.IsActive == true).ToList().Count(),
+                Children = db.Children.Where(x => x.IsActive == true).ToList().Count(),
+                People = db.People.Where(x => x.IsActive == true).ToList().Count(),
+                FamilyVisits = db.FamilyVisits.ToList().Count()
+            };
+
+            return View(systemDashboard);
         }
 
     }
+
+
+
 }
