987c09b43db49322d274fa68f67be010eed2d9e7
Michael Malak <m.malakrizk@gmail.com>
2021-01-19 12:58:17 +0200

    Medical Services and Contracts

---
+++ StAbraamFamily/Controllers/MedicalContractsController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StAbraamFamily.Models;
+
+namespace StAbraamFamily.Controllers
+{
+    public class MedicalContractsController : Controller
+    {
+        private StAbraamEntities db = new StAbraamEntities();
+
+        // GET: MedicalContracts
+        public async Task<ActionResult> Index()
+        {
+            var medicalContracts = db.MedicalContracts.Include(m => m.Clinic).Include(m => m.Hospital).Include(m => m.MedicalService);
+            return View(await medicalContracts.ToListAsync());
+        }
+
+        // GET: MedicalContracts/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MedicalContract medicalContract = await db.MedicalContracts.FindAsync(id);
+            if (medicalContract == null)
+            {
+                return HttpNotFound();
+            }
+            return View(medicalContract);
+        }
+
+        // GET: MedicalContracts/Create
+        public ActionResult Create()
+        {
+            ViewBag.ClinicID = new SelectList(db.Clinics, "ID", "ClinicName");
+            ViewBag.HospitalID = new SelectList(db.Hospitals, "ID", "HospitalName");
+            ViewBag.MedicalServiceID = new SelectList(db.MedicalServices, "ID", "MedicalService1");
+            return View();
+        }
+
+        // POST: MedicalContracts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ID,MedicalServiceID,HospitalID,ClinicID,Notes,IsFinished,Percentage,EntryDate,EnteredBy")] MedicalContract medicalContract)
+        {
+            if (ModelState.IsValid)
+            {
+                db.MedicalContracts.Add(medicalContract);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ClinicID = new SelectList(db.Clinics, "ID", "ClinicName", medicalContract.ClinicID);
+            ViewBag.HospitalID = new SelectList(db.Hospitals, "ID", "HospitalName", medicalContract.HospitalID);
+            ViewBag.MedicalServiceID = new SelectList(db.MedicalServices, "ID", "MedicalService1", medicalContract.MedicalServiceID);
+            return View(medicalContract);
+        }
+
+        // GET: MedicalContracts/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MedicalContract medicalContract = await db.MedicalContracts.FindAsync(id);
+            if (medicalContract == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ClinicID = new SelectList(db.Clinics, "ID", "ClinicName", medicalContract.ClinicID);
+            ViewBag.HospitalID = new SelectList(db.Hospitals, "ID", "HospitalName", medicalContract.HospitalID);
+            ViewBag.MedicalServiceID = new SelectList(db.MedicalServices, "ID", "MedicalService1", medicalContract.MedicalServiceID);
+            return View(medicalContract);
+        }
+
+        // POST: MedicalContracts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "ID,MedicalServiceID,HospitalID,ClinicID,Notes,IsFinished,Percentage,EntryDate,EnteredBy")] MedicalContract medicalContract)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(medicalContract).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ClinicID = new SelectList(db.Clinics, "ID", "ClinicName", medicalContract.ClinicID);
+            ViewBag.HospitalID = new SelectList(db.Hospitals, "ID", "HospitalName", medicalContract.HospitalID);
+            ViewBag.MedicalServiceID = new SelectList(db.MedicalServices, "ID", "MedicalService1", medicalContract.MedicalServiceID);
+            return View(medicalContract);
+        }
+
+        // GET: MedicalContracts/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MedicalContract medicalContract = await db.MedicalContracts.FindAsync(id);
+            if (medicalContract == null)
+            {
+                return HttpNotFound();
+            }
+            return View(medicalContract);
+        }
+
+        // POST: MedicalContracts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            MedicalContract medicalContract = await db.MedicalContracts.FindAsync(id);
+            db.MedicalContracts.Remove(medicalContract);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ StAbraamFamily/Controllers/MedicalServicesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StAbraamFamily.Models;
+
+namespace StAbraamFamily.Controllers
+{
+    public class MedicalServicesController : Controller
+    {
+        private StAbraamEntities db = new StAbraamEntities();
+
+        // GET: MedicalServices
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.MedicalServices.ToListAsync());
+        }
+
+        // GET: MedicalServices/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MedicalService medicalService = await db.MedicalServices.FindAsync(id);
+            if (medicalService == null)
+            {
+                return HttpNotFound();
+            }
+            return View(medicalService);
+        }
+
+        // GET: MedicalServices/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MedicalServices/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ID,MedicalService1,Notes,IsActive")] MedicalService medicalService)
+        {
+            if (ModelState.IsValid)
+            {
+                db.MedicalServices.Add(medicalService);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(medicalService);
+        }
+
+        // GET: MedicalServices/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MedicalService medicalService = await db.MedicalServices.FindAsync(id);
+            if (medicalService == null)
+            {
+                return HttpNotFound();
+            }
+            return View(medicalService);
+        }
+
+        // POST: MedicalServices/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "ID,MedicalService1,Notes,IsActive")] MedicalService medicalService)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(medicalService).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(medicalService);
+        }
+
+        // GET: MedicalServices/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MedicalService medicalService = await db.MedicalServices.FindAsync(id);
+            if (medicalService == null)
+            {
+                return HttpNotFound();
+            }
+            return View(medicalService);
+        }
+
+        // POST: MedicalServices/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            MedicalService medicalService = await db.MedicalServices.FindAsync(id);
+            db.MedicalServices.Remove(medicalService);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
