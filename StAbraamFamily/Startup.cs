5610221e14c173cef957f43fd9592741128fad55
Michael Malak <mattallah@rasan.co>
2021-08-21 23:10:18 +0200

    Revert "Add Link in Layout"
    This reverts commit 49f86e7f97559c3eeeda946f190b473791c24ae4.

--- StAbraamFamily/Startup.cs
+++ StAbraamFamily/Startup.cs
@@ -1,8 +1,5 @@
-﻿using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
-using Microsoft.Owin;
+﻿using Microsoft.Owin;
 using Owin;
-using StAbraamFamily.Models;
 
 [assembly: OwinStartupAttribute(typeof(StAbraamFamily.Startup))]
 namespace StAbraamFamily
@@ -12,23 +9,6 @@ namespace StAbraamFamily
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
-            //createRolesandUsers();
-        }
-
-        private void createRolesandUsers()
-        {
-            ApplicationDbContext context = new ApplicationDbContext();
-
-            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
-            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
-
-            // creating Creating Manager role     
-            if (!roleManager.RoleExists("HealthManagment"))
-            {
-                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
-                role.Name = "HealthManagment";
-                roleManager.Create(role);
-            }
         }
     }
 }
